c56bda82ecfb420c31aa28b284623b7c344853d5
raohai.rh <surgesoft@gmail.com>
2019-09-19 11:32:28 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":4194304,"name":"Callback","url":"globals.html#callback","classes":"tsd-kind-type-alias"},{"id":1,"kind":65536,"name":"__type","url":"globals.html#callback.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"Callback"},{"id":2,"kind":64,"name":"UseG2","url":"globals.html#useg2","classes":"tsd-kind-function tsd-has-type-parameter"}]};
